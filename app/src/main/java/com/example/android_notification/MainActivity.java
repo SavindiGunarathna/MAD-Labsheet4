282936c47b680dddda662b8c8f4dd7a12e0c443e
sachini.nk1996@gmail.com <savindigunarathna222@gmail.com>
2020-03-27 08:18:09 +0530

    Android Notification - MAD Lab 4

---
+++ app/src/main/java/com/example/android_notification/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.android_notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String CHANNEL_ID = "notification";
+
+    Button sign;
+    EditText value;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sign = findViewById(R.id.button);
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                value = (EditText) findViewById(R.id.editText);
+                String name = value.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,Register.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("Click on this to Register")
+                        .setContentText("Hello " + name + " ! Welcome to the MAD team")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+
+                notificationManager.notify(0, builder.build());
+            }
+        });
+
+
+
+
+
+    }
+}
